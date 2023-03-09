219adc73e8dbd2b3ed9d3be01682b3f53102f81e
JacksonCrawford <jacksoncrawford@protonmail.com>
2023-03-08 17:11:51 -0800

    add automation and restructure helper functions

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -6,6 +6,9 @@ import (
     "flag"
     "math/rand"
     "time"
+    "log"
+    "io"
+    "strings"
 )
 
 func Usage() {
@@ -31,6 +34,7 @@ func NotYetImplemented(method string) {
     panic((method + " not yet implemented"))
 }
 
+/* Helper method to check if a flag has been raised */
 func IsFlagRaised(flag_name string) bool {
     found := false
     flag.Visit(func(f *flag.Flag) {
@@ -51,5 +55,66 @@ func RandomString (length int) string {
     rand.Read(byteArr)
     // Return the random string
     return fmt.Sprintf("%x", byteArr)[2 : length+2]
+}
+
+/* Helper method to crash if errors exist */
+func FatalErrorCheck(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+/* Helper method to output present errors but not crash */
+func NonfatalErrorCheck(err error) {
+    if err != nil {
+        log.Print(err)
+    }
+}
+
+/* Helper method to read a file and return it as a string */
+func readFile (infile string) string {
+    // Open the current file and generate reader
+    f, err := os.Open(infile)
+    FatalErrorCheck(err)
+    defer f.Close()
+
+    // Read the current file
+    content, err := io.ReadAll(f)
+    // Crash if error
+    FatalErrorCheck(err) 
+
+    return string(content)
+}
+
+/* Helper method to write a file */
+func writeFile (outfile, data string) {
+    // Create the file 
+    f, err := os.Create(outfile)
+    // Crash if error
+    FatalErrorCheck(err)
+    // Close the file with defer
+    defer f.Close()
+
+    // Write the data
+    f.WriteString(data)
+}
+
+/* Method to inject a CA bundle into a YAML file */
+func InjectYamlCA (target, template, injectable string) {
+    // Read and store file with tls data
+    content := readFile(injectable)
+    // Read and store file to inject tls into
+    config := readFile(template)
+
+    // Remove unnecessary prefix from content
+    content = strings.TrimPrefix(content, ">> MutatingWebhookConfiguration caBundle:")
+    // Indent content correctly
+    content = strings.ReplaceAll(content, "\n", "\n                ")
+
+    // Insert the content into the config file string
+    config = strings.Replace(config, "caBundle: |", "caBundle: |\n                " + content, 1)
+
+    // Now write to file
+    writeFile(target, config)
 }
 
