0d7008db716833278dff51dc516d821bcf9a1e04
JacksonCrawford <jacksoncrawford@protonmail.com>
2023-03-26 13:26:09 -0700

    update log messages

--- pkg/webhook/validate.go
+++ pkg/webhook/validate.go
@@ -2,12 +2,10 @@ package webhook
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/usfca-cs490/admissions-webhook/pkg/dashboard"
 	"github.com/usfca-cs490/admissions-webhook/pkg/util"
 	"log"
 	"os/exec"
-	"time"
 
 	corev1 "k8s.io/api/core/v1"
 )
@@ -28,14 +26,14 @@ type Vulns struct {
 	Severity   string `json:"severity"`
 }
 
-// GenerateSBOM generates an SBOM from an image and stores it in an argued path (currently: ./pkg/sboms/<filename>)
-func GenerateSBOM(outfile, image string) {
+// generateSBOM generates an SBOM from an image and stores it in an argued path (currently: ./pkg/sboms/<filename>)
+func generateSBOM(outfile, image string) {
 	// Create and run command
-	log.Print("Image: " + image)
+    log.Print("validate.go: GenerateSBOM -> creating SBOM for " + image + "...")
 	out, err := exec.Command("syft", "-o", "json", image).Output()
 	// Crash if there are any errors
-	util.FatalErrorCheck(err)
-	log.Print("Past SBOM")
+	util.FatalErrorCheck(err, true)
+	log.Print("validate.go: GenerateSBOM -> created SBOM for " + image + " and stored at " + outfile)
 
 	// Write output to file
 	util.WriteFile(outfile, string(out))
@@ -45,21 +43,20 @@ func GenerateSBOM(outfile, image string) {
 // writes the grype evaluation results to a json file, reads the evaluation into the special struct,
 // checks if that image breaks the security rules (has "Critical" rated CVEs),
 // and returns false if the rules are not broken
-func runGrypeOnSingleImage(sbomFile string, imageName string) bool {
-	timeName := formatTime()
+func evaluateImage(sbomFile string, imageName string) bool {
+	timeName := util.FormatTime()
 	//EX: evals/nginx_eval_2023-3-20_17-57-50.json
 	outFile := "evals/" + imageName + "_eval_" + timeName + ".json"
 
 	// run grype command
 	givenSBOM := "sbom:./" + sbomFile
 	// To scan an SBOM: grype sbom:./example.json
-	log.Print("pre grype")
+    log.Print("validate.go: evaluateImage -> running grype on SBOM at " + givenSBOM)
 	out, err := exec.Command("grype", givenSBOM, "-o", "json").Output()
-	log.Print("Post grype")
 	// Crash if there are any errors
-	util.NonfatalErrorCheck(err)
+	util.FatalErrorCheck(err, true)
+    log.Print("validate.go: evaluateImage -> grype ran successfully and stored output in " + outFile)
 
-	log.Print("Outfile name:" + outFile)
 	// Write output to file
 	util.WriteFile(outFile, string(out))
 
@@ -80,14 +77,6 @@ func runGrypeOnSingleImage(sbomFile string, imageName string) bool {
 	return false
 }
 
-// formatTime generates a string of the current time for naming files
-func formatTime() string {
-	// make filename based off of current time (lazy but effective)
-	currTime := time.Now()
-	// format is from the docs, plz don't change it bc the date is based on the underlying schema used by the Format()
-	fileName := currTime.Format("2006-1-2_15-4-5")
-	return fileName
-}
 
 // TODO: add all functionality
 // checkPodImages pulls out all images from a pod struct and sends them to the DB interface,
@@ -112,15 +101,14 @@ func checkPodImages(pod *corev1.Pod) (dashboard.DashboardUpdate, error) {
 		imageSlice[counter] = containers[counter].Image
 
 		// TODO: don't do any this here, this is just the proof of concept
-		timeRaw := formatTime()
+		timeRaw := util.FormatTime()
 		// EX: sboms/nginx_sbom_2023-3-20_17-57-50.json
 		sbomName := "sboms/" + containers[counter].Image + "_sbom_" + timeRaw + ".json"
-		GenerateSBOM(sbomName, containers[counter].Image)
-		log.Print("Made SBOM")
+		generateSBOM(sbomName, containers[counter].Image)
 		// we could immediately stop and break here, but I think it's worth checking all the images in the pod
 		// TODO: change this so that it makes a list of the images that failed and try to come up with a way that can say what CVEs caused the failure? idk
-		grypeRes := runGrypeOnSingleImage(sbomName, containers[counter].Image)
-		log.Print("ran grype")
+		grypeRes := evaluateImage(sbomName, containers[counter].Image)
+        log.Print("validate.go: checkPodImages -> successfully evaluated vulnerabilities")
 		if grypeRes == true {
 			failure = true
 		}
@@ -128,16 +116,16 @@ func checkPodImages(pod *corev1.Pod) (dashboard.DashboardUpdate, error) {
 		counter++
 	}
 
-	log.Print("out of loop")
 	singleImage := pod.Spec.Containers[0].Image
-	fmt.Println(singleImage)
 
 	// TODO: pass each image to the DB interface from here and receive the grype results
 
 	// currently rejects any pod with an image containing a Critical level CVE
 	if failure {
+        log.Print("validate.go: checkPodImages -> " + singleImage + " was denied")
 		return dashboard.DashboardUpdate{Denied: true}, nil
 	}
 
+    log.Print("validate.go: checkPodImages -> " + singleImage + " was accepted")
 	return dashboard.DashboardUpdate{Denied: false}, nil
 }
