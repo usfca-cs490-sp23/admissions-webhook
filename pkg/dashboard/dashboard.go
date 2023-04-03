7faa2e8746a5e54f9e40a323ca50f9187a4e5b58
Haley Lenander <halenander@gmail.com>
2023-04-02 21:07:13 -0700

    struct change

--- pkg/dashboard/dashboard.go
+++ pkg/dashboard/dashboard.go
@@ -10,9 +10,8 @@ import (
 
 // DashboardUpdate TODO: Return a DashboardUpdate struct with the result of checking the internals of the pod
 type DashboardUpdate struct {
-	// TODO: make a list of all SBOMs from the pod to add to DB / check with grype
-	// TODO:
-	Denied bool
+	CVEList map[string][]string
+	Denied  bool
 }
 
 // DashInit initiates the dashboard on user's local computer
